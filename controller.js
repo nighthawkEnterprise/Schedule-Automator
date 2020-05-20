599d9763ba4a22700f2a323f7d8250e4a6922ed3
Nithin Kumar Moorthy <nithinmoorthy11@gmail.com>
2020-05-19 21:37:29 -0700

    Updated Package JSON file

--- controller.js
+++ controller.js
@@ -51,20 +51,82 @@ module.exports.getGuests = async (dataObj) => {
   console.log("URL: ", url);
   return await axios.get(`https://api.zenoti.com/v1/guests?center_id=${center_Id}`, config)
     .then(response => {
-        // console.log("in response of guest: ", response);
         const guests = response.data;
         // console.log("Guests in get Guests: ", guests);
         return guests;
     })
 }
-module.exports.getBookingId = async (dataObj) => {
-  // console.log('bookingID Called: ');
+module.exports.getTherapists = async (dataObj) => {
   const center_Id = dataObj.centers[1].id;
-  const guest_Id = dataObj.guests[0].id;
-  const service_Id = dataObj.services[0].id;
-  // console.log("GuestID: ", dataObj.guests[0].id);
-  // console.log("ServiceID: ", dataObj.services[0].id);
-  const date = "2020-03-26";
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  }
+  return await axios.get(`https://api.zenoti.com/v1/centers/${center_Id}/therapists`, config)
+    .then(response => {
+        const therapists = response.data.therapists;
+        // console.log("Guests in get Guests: ", guests);
+        return therapists;
+    })
+
+}
+
+module.exports.getMemberships = async (dataObj) => {
+  const center_Id = dataObj.centers[1].id;
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  }
+  return await axios.get(`https://api.zenoti.com/v1/centers/${center_Id}/memberships`, config)
+    .then(response => {
+        const memberships = response.data.memberships;
+        // console.log("Memberships in get Memberships: ", memberships);
+        return memberships;
+    })
+
+}
+module.exports.getBookingId = async (dataObj) => {
+  // // console.log('bookingID Called: ');
+  // // console.log("DATAOBJ:::::::: ", dataObj);
+ var index = Math.floor(Math.random() * 10);
+ console.log("GUESTIDINDEX: ", index);
+  const center_Id = dataObj.centers[1].id;
+  const guest_Id = dataObj.guests[index].id;
+  const service_Id = dataObj.services[index].id;
+  console.log('Index: ', index, '  GUESTS: ', dataObj.guests[index].personal_info.first_name + " " + dataObj.guests[index].personal_info.last_name);
+  console.log('Index: ', index, ' Services: ', dataObj.services[index].name);
+  // console.log("SERVICES: ", dataObj.services);
+  // console.log('GUESTS: ', dataObj.guests[0].personal_info.first_name + " " + dataObj.guests[0].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[1].personal_info.first_name + " " + dataObj.guests[1].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[2].personal_info.first_name + " " + dataObj.guests[2].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[3].personal_info.first_name + " " + dataObj.guests[3].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[4].personal_info.first_name + " " + dataObj.guests[4].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[5].personal_info.first_name + " " + dataObj.guests[5].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[6].personal_info.first_name + " " + dataObj.guests[6].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[7].personal_info.first_name + " " + dataObj.guests[7].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[8].personal_info.first_name + " " + dataObj.guests[8].personal_info.last_name);
+  // console.log('GUESTS: ', dataObj.guests[9].personal_info.first_name + " " + dataObj.guests[9].personal_info.last_name);
+  dataObj.centerId = center_Id;
+  dataObj.guestId = guest_Id;
+  dataObj.service_Id = service_Id;
+  // console.log("IN BOOKING ID: ", dataObj.guestId);
+  // console.log("Center_ID: ", dataObj.centerId);
+  // // console.log("center_ID: ", center_id, ":", dataObj.centers[1].name);
+  // // console.log("Guest_ID: ", guest_id, ":", dataObj.guests[0].id);
+  // // console.log()
+  // // console.log("GuestID: ", dataObj.guests[0].id);
+  // // console.log("ServiceID: ", dataObj.services[0].id);
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+ (today.getDate() + 4);
+  console.log("DATE: ", date);
+
+
   const config = {
     headers: {
       'application_name' : "zdemo",
@@ -96,7 +158,7 @@ module.exports.getBookingId = async (dataObj) => {
   })
 }
 module.exports.getReservationSlots = async (dataObj) => {
-  // console.log("In get reservation ID controller");
+  // // console.log("In get reservation ID controller");
   const config = {
     headers: {
       'application_name' : "zdemo",
@@ -105,10 +167,10 @@ module.exports.getReservationSlots = async (dataObj) => {
     }
   };
   const bookingId= dataObj.bookingId;
-  console.log('BookingID in get Reservation: ', bookingId)
-  return await axios.get(`https://api.zenoti.com/v1/bookings/${bookingId}/slots`, config)
+ // console.log("Booking ID: ", dataObj.bookingId);
+  return await axios.get(`https://api.zenoti.com/v1/bookings/${bookingId}/slots?check_future_day_availability=true`, config)
     .then(response => {
-//    console.log("reservationID: ", response.data);
+      // console.log("reservationID: ", response.data);
       const reservationSlots = response.data.slots;
       return reservationSlots;
     })
@@ -116,6 +178,7 @@ module.exports.getReservationSlots = async (dataObj) => {
 }
 
 module.exports.reserveSlot = async (dataObj) => {
+  // // console.log("In Reserve Slot Controller");
   const config = {
     headers: {
       'application_name' : "zdemo",
@@ -124,22 +187,23 @@ module.exports.reserveSlot = async (dataObj) => {
     }
   };
   const bookingId = dataObj.bookingId;
-  // console.log("RESERVATION SLOTS: ", dataObj.reservationSlots[0].Time);
-  const reservationSlot = dataObj.reservationSlots[2].Time;
+  // console.log("dataOBJ: ", dataObj.reservationSlots);
+  console.log("RESERVATION SLOTS: ", dataObj.reservationSlots[0].Time);
+  const reservationSlot = dataObj.reservationSlots[0].Time;
   const data = {
     "slot_time": reservationSlot
   }
 
-  console.log("Reservation Slot: ", reservationSlot);
+  // // console.log("Reservation Slot: ", reservationSlot);
   return await axios.post(`https://api.zenoti.com/v1/bookings/${bookingId}/slots/reserve`, data, config)
     .then(response => {
       const reservationId = response.data.reservation_id;
-      console.log('Response for Reservation Slot: ', reservationId);
+      // // // console.log('Response for Reservation Slot: ', reservationId);
       return reservationId;
     })
 }
 module.exports.confirmBooking = async dataObj => {
-  console.log("In Confirm Booking");
+  // // // console.log("In Confirm Booking");
   const config = {
     headers: {
       'application_name' : "zdemo",
@@ -148,13 +212,105 @@ module.exports.confirmBooking = async dataObj => {
     }
   };
   const bookingId = dataObj.bookingId;
-  console.log("booking token", dataObj.tokenKey.access_token)
-  console.log('BookingID in confirm Booking: ', bookingId)
+  // // // console.log("booking token", dataObj.tokenKey.access_token)
+  // // // console.log('BookingID in confirm Booking: ', bookingId)
+  // // // console.log('dataObj')
   return await axios.post(`https://api.zenoti.com/v1/bookings/${bookingId}/slots/confirm`, null, config)
     .then(response => {
-//    console.log("reservationID: ", response.data);
       const invoiceId = response.data;
       return invoiceId;
     })
     .catch(err => console.log("error getting booking", err))
+}
+module.exports.retrieveCards = async dataObj => {
+  // console.log("IN RETRIEVE CARDS CONTROLLER");
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  };
+  // console.log("CONFIG: ", config);
+  // console.log("GUEST_ID: ", dataObj.guestId);
+  // console.log("CENTER_ID: ", dataObj.centerId);
+  return await axios.get(`https://api.zenoti.com/v1/guests/${dataObj.guestId}/accounts?center_id=${dataObj.centerId}`,config)
+    .then(response => {
+      // console.log("ACCOUNTS: ", response.data.accounts);
+      return response.data.accounts;
+    })
+    .catch(err => console.log("error getting accounts", err))
+
+}
+module.exports.getProducts = async (dataObj) => {
+  const center_Id = dataObj.centers[1].id;
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  }
+  return await axios.get(`https://api.zenoti.com/v1/centers/${center_Id}/products`, config)
+    .then(response => {
+        const products = response.data.products;
+        // console.log("Products in get Products: ", products);
+        return products;
+    })
+}
+module.exports.addProducts = async dataObj => {
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  };
+  var index = Math.floor(Math.random() * 10);
+  // console.log("DataObj.products.id: ", dataObj.products[index]);
+  var data = {
+    products: [
+      {
+        id: dataObj.products[index].id,
+        quantity: 3,
+        sale_by_id: dataObj.therapists[index].id,
+      }
+    ]
+  }
+  const invoiceId = dataObj.invoiceId;
+  // console.log("Products: ", dataObj.products);
+  console.log("Product ID of Sold: ", dataObj.products[index].id);
+  console.log("Product Name of Sold: ", dataObj.products[index].name);
+  console.log("Therapist id: ", dataObj.therapists[index].id);
+  console.log("Therapist id: ", dataObj.therapists[index].personal_info.first_name);
+  console.log("INVOICEID: ", invoiceId);
+
+  return await axios.put(`https://api.zenoti.com/v1/invoices/${invoiceId}/products`, data, config)
+    .then(response => {
+        console.log('Added Product to Invoice, RESPONSE: ', response.data);
+        return response.data.invoice_id;
+    }).catch(err => console.log("error paying card", err))
+
+}
+module.exports.addPayment = async dataObj => {
+  const config = {
+    headers: {
+      'application_name' : "zdemo",
+      'application_version' : "11.43",
+      'Authorization' : 'bearer ' + dataObj.tokenKey.access_token
+    }
+  };
+  const invoiceId = dataObj.invoiceId;
+  // console.log("INVOICE_ID::::: ", invoiceId);
+  const data = {
+    account_id: dataObj.accountId,
+    center_id: dataObj.centerId
+  };
+  // console.log("DATA: ", data);
+  return await axios.post(`https://api.zenoti.com/v1/invoices/${invoiceId}/online_payments`, data, config)
+    .then(response => {
+        // console.log('RESPONSE: ', response.data);
+        return response.data.invoice_transaction_id;
+    }).catch(err => console.log("error paying card", err))
+
 }
